7bd1a150c1b1eb36f1f125ab7b0024f1f75822de
spaceorion <109016495+spaceorion@users.noreply.github.com>
2022-11-05 13:29:42 +0530

    Update routing.py

--- myapp/routing.py
+++ myapp/routing.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path,re_path
 
 from . import consumers
 
 websocket_urlpatterns = [
     path('ws/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
+    re_path(r'^ws/chat/(?P<room_name>[\w-]+)/', consumers.ChatConsumer.as_asgi()),
     
 
 ]
